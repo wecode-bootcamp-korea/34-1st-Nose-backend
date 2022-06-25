5cd27dab2dc29c5a10552d0cf485e006b0fabe87
sangwoong03 <sangwoong03@gmail.com>
2022-06-25 17:01:00 +0900

    Add: completed product list view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import View
+from django.http  import JsonResponse
 
-# Create your views here.
+from products.models import Product, ThumbNailImage
+
+class ProductListView(View) :
+    def get(self, request) :
+        try:
+            products = Product.objects.all()
+
+            result  = [
+                {
+                    "product_id"    : product.id,
+                    "name"          : product.name,
+                    "price"         : product.price,
+                    "thumbnail_img" : ThumbNailImage.objects.get(product_id=product.id).thumb_nail_img_url,
+                    "tags"          : [ tag.name for tag in product.tags.all() ]
+                } for product in products
+            ]
+
+            return JsonResponse({"perfume_list": result}, status=200)
+
+        except Product.DoesNotExist:
+            return JsonResponse({"message": "NONE_PRODUCT"}, status=400)
