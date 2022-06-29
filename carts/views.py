36ee035d37dd19dd64f069073d255e27330b68c8
kjy0627 <jkim8@stevens.edu>
2022-06-29 16:53:33 +0900

    Fix: Cart(get,delete)

--- carts/views.py
+++ carts/views.py
@@ -3,7 +3,6 @@ import json
 from django.views       import View
 from django.http        import JsonResponse
 
-from core.models        import TimeStampModel
 from users.models       import User
 from products.models    import ThumbNailImage
 from carts.models       import Cart
@@ -17,52 +16,54 @@ class CartView(View):
             quantity        = data['quantity']
             product_id      = data['product_id']
             user            = request.user
-
-            Cart.objects.get_or_create(
+    
+            cart, is_created = Cart.objects.get_or_create(
                 user            = user, 
                 product_id      = product_id,
-                quantity        = quantity
+                defaults        = {
+                    "quantity" : quantity
+                }
             )
-            
+
+            if not is_created:
+                cart.quantity += quantity
+
+            cart.save()
+
             return JsonResponse ({'message':'SUCCESS'}, status=200)
         except KeyError:
             return JsonResponse ({'message':'KEY_ERROR'}, status=400)
-        except User.DoesNotExist:
-            return JsonResponse ({'message':'INVALID_USER'}, status=400)
 
     @token_decorator
     def get(self, request):
         try:
             user    = request.user
             carts   = Cart.objects.filter(user_id=user.id)
-            
-            results = []
-            for cart in carts:
-                results.append(
-                    {
+            results = [{
                     'product_id'   : cart.product.id,
                     'product_name' : cart.product.name,
-                    'image_url'    : ThumbNailImage.thumb_nail_img_url,
+                    'image_url'    : cart.product.thumbnailimage_set.get(product_id=cart.product.id).thumb_nail_img_url,
                     'quantity'     : cart.quantity,
                     'cart_id'      : cart.id
-                    }
-                )
+                    }for cart in carts]
 
-            return JsonResponse({'results': 'results'}, status=200)
+            return JsonResponse({'results': results}, status=200)
 
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
         except ThumbNailImage.DoesNotExist:
             return JsonResponse({'message': 'NO_IMAGE'}, status=401) 
 
     @token_decorator
     def delete(self, request):
         try:
-            user = request.user
-            cart = Cart.objects.get(user_id=user.id)
-            
-            cart.delete()
+            user     = request.user
+            cart_ids = request.GET.getlist('cart_id')
+            carts    = Cart.objects.filter(id__in=cart_ids, user_id=user.id)
 
-            return JsonResponse({'message': 'NO_CONTENT'}, status=200)
+            if not carts:
+                return JsonResponse({'message' : 'NOT EXIST CART'}, status=400)
+
+            carts.delete()
+
+            return JsonResponse({'message': 'DELETE SUCCESS'}, status=201)
         except Cart.DoesNotExist:
             return JsonResponse({'message': 'Cart.DoesNotExist'}, status=404)                  
